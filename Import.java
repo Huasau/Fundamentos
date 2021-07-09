22ed48ed006afc095b0f67a71e4641079d3dca23
Amauri Lucas <60305034+Huasau@users.noreply.github.com>
2021-07-09 18:17:13 -0300

    Importando a Data de Hoje / Agora
    Saber o dia, a hora e a semana

---
+++ Import.java
@@ -0,0 +1,18 @@
+package fundamentos;
+
+import java.util.Date;
+
+public class Import {
+public static void main(String[] args) {
+	String b = "Boa Tarde";
+	System.out.println(b);
+	
+	String s = "Bom Dia!";
+	System.out.println(s);
+	
+	// Importando a Data de Hoje / Agora
+	Date d = new Date();
+	System.out.println(d);
+	
+}
+}
